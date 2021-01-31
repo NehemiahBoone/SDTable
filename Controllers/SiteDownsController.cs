eead12d6463b6ea454b6420db46636372097a4b9
Nehemiah Boone <nehemiah.boone@gmail.com>
2021-01-31 13:36:31 -0700

    post and get

--- Controllers/SiteDownsController.cs
+++ Controllers/SiteDownsController.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SDTable.Models;
 using SDTable.Services;
 
 namespace SDTable.Controllers
@@ -11,6 +16,36 @@ namespace SDTable.Controllers
     public SiteDownsController(SiteDownsService service)
     {
       _service = service;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<SiteDown>> GetAll()
+    {
+      try
+      {
+        return Ok(_service.GetAll());
+      }
+      catch (System.Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<SiteDown>> PostSiteDown([FromBody] SiteDown newSD)
+    {
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        newSD.CreatorId = userInfo.Id;
+        SiteDown created = _service.PostSiteDown(newSD);
+        return Ok(created);
+      }
+      catch (System.Exception e)
+      {
+        return BadRequest(e.Message);
+      }
     }
   }
 }

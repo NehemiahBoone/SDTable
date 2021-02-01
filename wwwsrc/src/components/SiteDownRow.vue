2b2ac2a3d8a56516c3b88b533df5638279ff836d
Nehemiah Boone <nehemiah.boone@gmail.com>
2021-02-01 08:29:17 -0700

    front end setup

---
+++ wwwsrc/src/components/SiteDownRow.vue
@@ -0,0 +1,15 @@
+<template>
+  <tr>
+    <th scope="row">{{ sitenum }}</th>
+    <td>{{ sitename }}</td>
+    <td>{{ cause }}</td>
+    <td>{{ solved }}</td>
+  </tr>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- wwwsrc/src/pages/Home.vue
+++ wwwsrc/src/pages/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="home">
-    <h1>Welcome</h1>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Site #</th>
+          <th scope="col">Site Name</th>
+          <th scope="col">Cause</th>
+          <th scope="col">Solved?</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import SiteDownRow from "../components/SiteDownRow.vue";
 export default {
   name: "home",
+  mounted() {
+    this.$store.dispatch("getSiteDowns");
+  },
+  computed: {
+    sitedowns() {
+      return this.$store.state.sitedowns;
+    },
+  },
+  components: {
+    SiteDownRow,
+  },
 };
 </script>
